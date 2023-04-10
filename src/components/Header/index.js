ab3efd572974cce335a3d1631383a1b1dd47f8b2
jvictort <jvteodorobrasil@gmail.com>
2023-04-10 02:29:14 -0300

    Componente Header

---
+++ src/components/Header/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import * as C from './styles';
+
+const Header = () => {
+  return (
+    <C.Container>
+      <C.Title>
+        <C.Header>Controle Financeiro</C.Header>
+      </C.Title>
+    </C.Container>
+  )
+}
+
+export default Header;
